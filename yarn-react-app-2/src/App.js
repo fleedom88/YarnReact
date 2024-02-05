361ecc95934b996ba087e25b8d9d68ac9742f519
fleedom88 <fleedom88@gmail.com>
2024-02-05 13:00:39 +0900

    리액트 공부

--- yarn-react-app-2/src/App.js
+++ yarn-react-app-2/src/App.js
@@ -3,9 +3,67 @@ import CreateUser from "./components/CreateUser";
 import UserList from "./components/UserList";
 
 function App() {
+  const [inputs, setInputs] = useState({
+    username:"",
+    email:"",
+  });
+
+  const [users, setUsers]= useState([
+    {id:1,
+    username: "cocoon",
+    email: "asd123@gmail.com",
+    },
+    {id:2,
+      username: "ultra",
+      email: "qwe123@gmail.com",
+    },
+    {id:3,
+      username: "hozae",
+      email: "zxc123@gmail.com",
+    },
+    ]);
+
+  const {username, email} = inputs;
+  const nextId = useRef(4);
+
+  const onChange = (e) =>{
+    const {name, value} = e.target;
+    setInputs({
+      ...inputs,
+      [name]:value,
+    });
+  };
+
+  const onCreate = () =>{
+    const user ={
+      id:nextId.current,
+      username,
+      email,
+    };
+
+    setUsers([...users], user);
+    //또는 setUsers(users.concat(user));
+
+    setInputs({
+      username:"",
+      email:"",
+    });
+
+    nextId.current += 1;
+  };
+
+  const onRemove = (id) =>{
+    setUsers(users.filter((user) => user.id !==id));
+  }
   return (
     <>
-      <UserList></UserList>
+      <CreateUser
+      username={username}
+      email={email}
+      onChange={onChange}
+      onCreate={onCreate}
+      ></CreateUser>
+      <UserList user={users} onRemove={onRemove}></UserList>
     </>
   );
 }
--- yarn-react-app-2/src/components/UserList.js
+++ yarn-react-app-2/src/components/UserList.js
@@ -1,36 +1,40 @@
 import React from "react";
 
-function User({user}){
+function User({user, onRemove}){
     return(
         <div>
             <b>{user.username}</b> <span>({user.email})</span>
+            <button
+            onClick={()=>{
+                onRemove(user.id);
+            }}
+            >삭제</button>
         </div>
     );
 }
 
-function UserList(){
-    const users=[
-        {
-            id:1,
-            username:"cocoon",
-            email:"asd123@gmail.com"
-        },
-        {
-            id:2,
-            username:"ultra",
-            email:"qwe123@gmail.com"
-        },
-        {
-            id:3,
-            username:"hozae",
-            email:"zxc123@gmail.com"
-        },
-    ]
-
+function UserList({users, onRemove}){
+    // const users=[
+    //     {
+    //         id:1,
+    //         username:"cocoon",
+    //         email:"asd123@gmail.com"
+    //     },
+    //     {
+    //         id:2,
+    //         username:"ultra",
+    //         email:"qwe123@gmail.com"
+    //     },
+    //     {
+    //         id:3,
+    //         username:"hozae",
+    //         email:"zxc123@gmail.com"
+    //     },
+    // ]
     return(
         <div>
-            {users.map((user,index)=>(
-                <User user={user} key={index}></User>
+            {users.map((user)=>(
+                <User user={user} key={user.id} onRemove={onRemove}></User>
             ))}
         </div>
     );
